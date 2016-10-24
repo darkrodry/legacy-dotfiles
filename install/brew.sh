b97869cee6090e2d83e9f179b61d52b489090cbd
Rodrigo De Frutos <darkrodry@gmail.com>
2016-10-25 00:27:22 +0200

    Install git, wget and zsh using brew

--- install/brew.sh
+++ install/brew.sh
@@ -1,3 +1,11 @@
 /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 brew update
 brew upgrade  
+
+apps=(
+  git
+  wget
+  zsh
+)
+
+brew install "${apps[@]}"

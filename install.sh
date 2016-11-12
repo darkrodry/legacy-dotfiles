ea6f36ba36bb92de2c6746f07e6f8367d9b5e831
Rodrigo De Frutos <darkrodry@gmail.com>
2016-11-12 23:49:14 +0100

    Update install.sh to autoinstall app for osx or linux (antergos)

--- install.sh
+++ install.sh
@@ -2,5 +2,11 @@
 
 DOTFILES_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
+if [ "$(uname)" == "Darwin" ]; then
+  . "$$DOFILES_DIR/osx/brew.sh"
+elif [ "$(uname)" == "Linux" ]; then
+  . "$DOTFILES_DIR/antergos/apps.sh"
+fi
+
 ln -sfv "$DOTFILES_DIR/git/.gitconfig" ~
 ln -sfv "$DOTFILES_DIR/git/.gitignore_global" ~

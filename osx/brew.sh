6341335003f9b17d582b77c9f0f57ed2325ed64e
Rodrigo De Frutos <darkrodry@gmail.com>
2017-01-08 21:57:13 +0100

    Add vim and oh-my-zsh dependencies to osx app script

--- osx/brew.sh
+++ osx/brew.sh
@@ -7,8 +7,12 @@ brew upgrade
 apps=(
   git
   pyenv
+  vim
   wget
   zsh
 )
 
 brew install "${apps[@]}"
+
+# Install oh-my-zsh
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"

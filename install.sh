4ae5d038c75e8dd1a52775188cd075af82049fb4
Rodrigo De Frutos <darkrodry@gmail.com>
2017-05-31 17:39:23 +0200

    Improve install script to make optional app instalation

--- install.sh
+++ install.sh
@@ -1,12 +1,32 @@
 #!/usr/bin/env sh
 
+INSTALL=false 
+UPDATE=true
 DOTFILES_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
-if [ "$(uname)" == "Darwin" ]; then
-  . "$DOTFILES_DIR/osx/brew.sh"
-elif [ "$(uname)" == "Linux" ]; then
-  . "$DOTFILES_DIR/antergos/apps.sh"
+while getopts 'iu' flag; do
+  case "${flag}" in
+    i) INSTALL=true ;;
+    u) UPDATE=true ;;
+    *) error "Unexpected option ${flag}" ;;
+  esac
+done
+
+if $INSTALL; then
+  echo "Installing first configuration"
+
+  if [ "$(uname)" == "Darwin" ]; then
+    echo "-> osX configuration"
+    . "$DOTFILES_DIR/osx/brew.sh"
+  elif [ "$(uname)" == "Linux" ]; then
+    echo "-> antergos configuration"
+    . "$DOTFILES_DIR/antergos/apps.sh"
+  fi
 fi
 
-ln -sfv "$DOTFILES_DIR/git/.gitconfig" ~
-ln -sfv "$DOTFILES_DIR/git/.gitignore_global" ~
+if $UPDATE; then 
+  echo "Installing dotfiles"
+
+  ln -sfv "$DOTFILES_DIR/git/.gitconfig" ~
+  ln -sfv "$DOTFILES_DIR/git/.gitignore_global" ~
+fi

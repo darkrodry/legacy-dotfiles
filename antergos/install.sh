ff24a1a0d8aa9c58ddd686b02836ea3679361435
Rodrigo De Frutos <darkrodry@gmail.com>
2018-11-01 19:56:14 +0100

    Remove old os specific config files

--- antergos/install.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env sh
-
-echo "\t-> antergos configuration"
-
-. "$DOTFILES_DIR/antergos/apps.sh"
--- osx/install.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env sh
-
-echo "\t-> osX configuration"
-
-. "$DOTFILES_DIR/osx/brew.sh"
